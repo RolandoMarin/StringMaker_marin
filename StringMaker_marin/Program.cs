4149ce37c695e9bcd740a8cbc378a2eab18f6b04
Rolando Marin <RolandoGalvanM@outlook.com>
2021-12-11 00:04:14 -0800

    Add project files.

---
+++ StringMaker_marin/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+// Rolando Marin
+// IT113
+// BEHAVIORS NOT IMPLIMENTED AND WHY: Very diffiult for me to implement to string. I tried multiple things and saddly gioven up :(
+namespace StringMaker_marin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            StringManager fancy = new StringManager();
+
+            Console.WriteLine("reverse: " + fancy.Reverse("Sunbeam Tiger" ));
+
+            //Console.WriteLine("\nTostring: " + fancy.Tostring("one two five four"));
+
+            Console.WriteLine("\nreverse with case: " + fancy.Reverse("Sunbeam Tiger", true));
+
+            Console.WriteLine("equals: " + fancy.Equals("Sunbeam Tiger"));
+
+            Console.WriteLine("\nis ABBA symetric?" + fancy.Symetric("ABBA"));
+
+            Console.WriteLine("\nis ABA symetric?" + fancy.Symetric("ABA"));
+
+            Console.WriteLine("\nis ABba symetric?" + fancy.Symetric("ABba"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Queue<StringManager> myqueue = new Queue<StringManager>();
+
+            //StringManager fancy = new StringManager("Dan");
+
+
+
+            //string hi = "Sunbeam Tiger";
+
+            //myqueue.Enqueue(new StringManager(hi));
+
+            //StringManager next = myqueue.Dequeue();
+
+            //Console.WriteLine(next.Reverse(hi));
+
+            //Console.WriteLine("Tostring: " + fancy.Equals("Dan"));
+
+
+
+
+
+
+
+            //Console.WriteLine("Enter a string to reverse: ");
+            //string rev =  Console.ReadLine();
+
+            //StringManager sm = new StringManager(rev);
+
+
+            //sm.Reverse(rev);
+
+            //Console.WriteLine(sm.Reverse(rev));
+
+
+            //Console.WriteLine("Enter a string to reverse & cases: ");
+
+            //string caseRev = Console.ReadLine();
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ StringMaker_marin/StringManager.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMaker_marin
+{
+    internal class StringManager
+    {
+        private string lastInput = "";
+        public string Reverse(string s) 
+        {
+        this.lastInput = s;
+        string anotherS = "";
+
+        Stack<char> charStack = new Stack<char>();
+        foreach(char c in s)
+            {
+                charStack.Push(c);
+            }
+        while(charStack.Count > 0)
+            {
+                anotherS += charStack.Pop();
+            }
+        return anotherS;
+        
+        }
+
+    //    public string Tostring (string s)
+    //    {
+
+    //        //was not succesfull 
+    //        int ASCII = 0;
+
+    //       foreach(char c in s)
+    //        {
+    //            ASCII += (int)c;
+    //        }
+    //        return ASCII % 5;
+        
+    //}
+
+
+        public string Reverse(string s,bool PreserveCaseLocation) {
+            this.lastInput = s;   
+
+            if(PreserveCaseLocation != true)
+            {
+                return Reverse(s);
+            }
+
+            Stack<int> index = new Stack<int>();
+
+            Stack<char>  anotherChars= new Stack<char>();
+
+            foreach (char c in s) 
+            if(char.IsUpper(c) == true)
+            {
+                index.Push(s.IndexOf(c));
+            }
+
+            char[] myarray = (Reverse(s).ToLower().ToCharArray());
+
+            while(index.Count > 0)
+            {
+                myarray[index.Peek()] = char.ToUpper(myarray[index.Pop()]);
+            }
+            return (string.Join("", myarray));
+
+        }
+
+        public bool Symetric(string s)
+        {
+            if (s == Reverse(s))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is string s && lastInput == s;
+        }
+
+
+
+
+
+
+
+        //    private string _s;
+        //    private bool _preserveCaseLocation;
+        //    public string Reverse(string s)
+        //    {
+        //        char[] myArr = s.ToCharArray();
+        //        Array.Reverse(myArr);
+        //        return new string(myArr);
+
+        //    }
+
+        //    public string Reverse(string s, bool preserveCaseLocation)
+        //    {
+        //        return Reverse(s, preserveCaseLocation);
+
+        //    }
+
+        //    public string s
+        //    {
+        //        get { return _s; }
+        //    }
+
+
+
+        //    public bool preserveCaseLocation
+        //    {
+        //        get { return _preserveCaseLocation; }
+
+        //    }
+
+
+
+
+        //    public StringManager(string s)
+        //    {
+        //        string _s = s;
+
+        //    }
+
+        //    public StringManager(string s, bool preserveCaseLocation)
+        //    {
+        //        string _s = s;
+        //        bool _preserveCaseLocation = preserveCaseLocation;
+
+        //}
+
+
+
+
+
+
+        //    public override string ToString()
+        //    {
+        //        return base.ToString();
+        //    }
+
+
+
+
+
+
+
+
+
+    }
+}
